901ed78c6528410e2ad215d843b5777abb3f3d3b
Sergio <sergio@kali.kali.local>
2022-07-16 14:07:03 +0200

    First files

---
+++ base-mbr.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# ln -sf /usr/share/zoneinfo/Europe/Zurich /etc/localtime
+# hwclock --systohc
+# sed -i '177s/.//' /etc/locale.gen
+# locale-gen
+# echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "KEYMAP=es" >> /etc/vconsole.conf
+echo "arch" >> /etc/hostname
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1       localhost" >> /etc/hosts
+echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
+# change password to the password you want
+echo root:password | chpasswd
+
+# You can remove the tlp package if you are installing on a desktop or vm
+
+pacman -S grub networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pulseaudio bash-completion openssh rsync reflector acpi acpi_call virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font sudo
+
+#tlp package is optional
+
+# pacman -S --noconfirm xf86-video-amdgpu
+# pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
+
+grub-install --target=i386-pc /dev/sdX # replace sdx with your disk name, not the partition
+grub-mkconfig -o /boot/grub/grub.cfg
+
+systemctl enable NetworkManager
+systemctl enable bluetooth
+systemctl enable cups.service
+systemctl enable sshd
+systemctl enable avahi-daemon
+# systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
+systemctl enable reflector.timer
+systemctl enable fstrim.timer
+systemctl enable libvirtd
+systemctl enable firewalld
+systemctl enable acpid
+
+useradd -m s3rgio
+# change password to the passwd you want
+echo s3rgio:password | chpasswd
+usermod -aG wheel s3rgio
+
+# (to do) add user to sudoers file
+
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Arch-base
+---
+This repository contains scripts to install the base of Arch faster
