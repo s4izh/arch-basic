b4aec3de356e2be4b0fbfaee62bc5915520da72e
sergio-sama <sergiosanz234@gmail.com>
2022-07-23 11:09:19 +0200

    added user and swap_size variables

--- base.sh
+++ base.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 # https://github.com/sergio-sama/arch-install
 
+# user name and swap size desired
+user=
+swap_size=
+
 # create swap file
 # found out (not me) that optimal swapfile size is:
 # swap_size = ram_size + sqrt(ram_size) 
-fallocate -l 6GB /swapfile
+fallocate -l $swap_size /swapfile
 chmod 600 /swapfile
 mkswap /swapfile
 swapon /swapfile
-echo "/swapfile	none	swap	default	0 0" >> etc/fstab
+echo "/swapfile	swap	swap	default	0 0" >> etc/fstab
 
 # set localtime
 ln -sf /usr/share/zoneinfo/Europe/Madrid /etc/localtime
@@ -34,8 +38,8 @@ pacman -S grub efibootmgr dosfstools mtools os-prober
 
 # if efi 
 # you should have /dev/boot_partition
-# mounted on /boot/EFI
-grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
+# mounted on /boot/efi
+grub-install --target=x86_64-efi ---efi-directory=/boot/efi -bootloader-id=GRUB 
 
 # mbr
 # grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
@@ -47,6 +51,12 @@ systemctl enable NetworkManager
 
 pacman -S network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g sudo
 #tlp is optional for laptop
+
+pacman -S grub efibootmgr networkmanager network-manager-applet dialog mtools dosfstools base-devel linux-headers bluez bluez-utils cups alsa-utils pulseaudio pulseaudio-bluetooth reflector xdg-utils xdg-user-dirs
+
+systemctl enable NetworkManager
+systemctl enable bluetooth
+systemctl enable cups
 
 # enable services intalled
 # systemctl enable NetworkManager
@@ -64,11 +74,11 @@ systemctl enable acpid
 
 
 # add user
-useradd -m s3rgio
-usermod -aG wheel,libvirt s3rgio #video,audio,optical,storage,libvrt groups are optional
+useradd -m $user
+usermod -aG wheel,libvirt $user #video,audio,optical,storage,libvrt groups are optional
 
 # sudo
-echo "s3rgio ALL=(ALL) ALL" > /etc/sudoers.d/00_s3rgio
+echo "s3rgio ALL=(ALL) ALL" > /etc/sudoers.d/00_$user
 # or EDITOR=vim visudo and uncomment %wheel ALL=(ALL:ALL)ALL
 
 echo "Installation finished, setup root and user passwords with passwd"
